14d2fc948f1c2c0de663d74521489abb4826cbd6
PeterNAylor <peter.naylor4@hotmail.co.uk>
2021-06-22 14:17:10 +0100

    df

--- src/main/java/com/pn/home/UserApplication.java
+++ src/main/java/com/pn/home/UserApplication.java
@@ -11,7 +11,6 @@ import com.pn.home.constants.AppConstants;
 @SpringBootApplication
 @RestController
 public class UserApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(UserApplication.class, args);
 	}
